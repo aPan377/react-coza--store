68e94d8673903220ac42afb6fcf1872dc05d01c0
Anatolii Panchenko <michael_frost_ng@gmail.com>
2023-02-07 17:13:21 +0200

    slider was added

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import Navbar from "./navbar/Navbar";
+import Slider from "./slider/Slider";
+import "../css/app.css";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Slider />
       {/* Categories */}
       {/* Products */}
       {/* Footer */}
