25ef34295dcad97465c0469217d996896f36fe48
Anatolii Panchenko <michael_frost_ng@gmail.com>
2023-02-07 21:53:42 +0200

    Slider was corrected

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 import Navbar from "./navbar/Navbar";
-import Slider from "./slider/Slider";
+import Slider from "./navbar/slider/Slider";
+import Categories from "./categories/Categories";
+
 import "../css/app.css";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Slider />
-      {/* Categories */}
+      <Categories />
       {/* Products */}
       {/* Footer */}
     </div>
